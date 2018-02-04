aaa11967f7e3f0ffe7634ed68ab2b151aa0d170c
zahramaida <35809078+zahramaida@users.noreply.github.com>
2018-02-04 16:28:59 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/android/zahramaida_1202150246_modul1/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.android.zahramaida_1202150246_modul1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText makanan;
+    EditText jumlah;
+
+    String Restoran,Menu;
+    int Porsi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        makanan = (EditText)findViewById(R.id.Makanan);
+        jumlah = (EditText)findViewById(R.id.jumlahPorsi);
+    }
+
+    public void eatbus(View view) {
+
+        Intent intent=new Intent(this, SecondActivity.class);
+        Restoran = "EATBUS";
+        Menu = makanan.getText().toString();
+        Porsi= Integer.parseInt(jumlah.getText().toString());
+        int Harga = 50000*Porsi;
+        Log.d("total harga","Rp."+Harga);
+        intent.putExtra("Restoran",Restoran); //mengembalikan data ke SecondActiviy
+        intent.putExtra("Menu",Menu);
+        intent.putExtra("Porsi",Porsi);
+        intent.putExtra("Harga",Harga);
+        startActivity(intent);
+
+    }
+
+    public void abnormal(View view) {
+
+        Intent intent=new Intent(this, SecondActivity.class);
+        Restoran = "ABNORMAL";
+        Menu = makanan.getText().toString();
+        Porsi = Integer.parseInt(jumlah.getText().toString());
+        int Harga = 30000*Porsi;
+        intent.putExtra("Restoran",Restoran); //mengembalikan data ke SecondActivity
+        intent.putExtra("Menu",Menu);
+        intent.putExtra("Porsi",Porsi);
+        intent.putExtra("Harga",Harga);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/android/zahramaida_1202150246_modul1/SecondActivity.java
@@ -0,0 +1,57 @@
+package com.example.android.zahramaida_1202150246_modul1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView Restoran;
+    TextView Menu;
+    TextView Porsi;
+    TextView Harga;
+
+    String resto,menu;
+    int portion,harga;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Restoran = (TextView) findViewById(R.id.Resto);
+        Menu = (TextView) findViewById(R.id.Menu);
+        Porsi = (TextView) findViewById(R.id.Portion);
+        Harga = (TextView) findViewById(R.id.Harga);
+        Intent intent = getIntent();
+
+        resto = intent.getStringExtra("Restoran");
+        menu = intent.getStringExtra("Menu");
+        portion = intent.getIntExtra("Porsi",0);
+        harga = intent.getIntExtra("Harga",0);
+
+        int hrg = Integer.valueOf(harga)*Integer.valueOf(portion);
+
+        Restoran.setText(resto);
+        Menu.setText(menu);
+        Porsi.setText(""+ portion);
+        Harga.setText(""+ harga);
+
+        if(hrg > 65000){
+            Toast toast = Toast.makeText(this, "Jangan disini makan malamnya, uang kamu tidak cukup", Toast.LENGTH_LONG);
+            toast.show();
+        } else {
+            Toast toast = Toast.makeText(this, "Disini aja makan malamnya", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+    }
+
+    public void back(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
